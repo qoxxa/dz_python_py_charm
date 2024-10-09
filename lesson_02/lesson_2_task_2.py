6823d188ee2df23d1929bf7c74805f230e5881c2
qoxxa <lkostya.p@gmail.com>
2024-10-09 21:55:42 +0300

    lesson_2_task_2.py

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -9,27 +9,3 @@ try:
 
 except ValueError:
     print("Введите целое число")
-
-
-
-
-
-
-    # def qater_of_year(month):
-    #     if 1 <= month <= 3:
-    #         return ("I квартал")
-    #     elif 4 <= month <= 6:
-    #         return ("II квартал")
-    #     elif 7 <= month <= 9:
-    #         return ("III квартал")
-    #     elif 10 <= month <= 12:
-    #         return ("IV квартал")
-    #     else:
-    #         return ("Неверно указан номер месяца")
-    #
-    # try:
-    #     month = int(input("Введите номер месяца (1-12): "))
-    #     print(qater_of_year(month))
-    #
-    # except ValueError:
-    #     print("Введите целое число")
