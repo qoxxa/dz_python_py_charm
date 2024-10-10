3d6eeb1d8ceebbd047d373d279d2ae6f51151e3e
qoxxa <lkostya.p@gmail.com>
2024-10-10 19:07:04 +0300

    lesson_2_task_4.py

--- lesson_02/lesson_2_task_3.py
+++ lesson_02/lesson_2_task_3.py
@@ -1,6 +1,8 @@
 import math
-def squre(a):
-    return math.ceil(a/1)
 
 a = float(input("Введите сторону квадрата: "))
+def squre(a):
+    a = a**2
+    b = math.ceil(a)
+    return b
 print("Площадь квадрата = ", squre(a))
